5d5d77168a732d9e696206e349c6a11f058e8243
Paulina Pavičić <ppavicic@racunarstvo.hr>
2026-06-13 22:34:02 +0200

    Add functional programming demo page and helpers

---
+++ ViewModels/FunctionalDemoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Obrasci.Models;
+
+namespace Obrasci.ViewModels
+{
+    public class FunctionalDemoViewModel
+    {
+        public List<Photo> SamplePhotos { get; set; } = new();
+        public List<Photo> FilteredPhotos { get; set; } = new();
+        public List<string> FileNames { get; set; } = new();
+        public long TotalBytes { get; set; }
+        public List<string> ParsedHashtags { get; set; } = new();
+        public Dictionary<string, int> CountByAuthor { get; set; } = new();
+        public string FilterDescription { get; set; } = string.Empty;
+    }
+}
